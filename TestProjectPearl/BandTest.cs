6b45fd6364a5b3c804fae4d068063046a9d04f76
leoggehrer <g.gehrer@htl-leonding.ac.at>
2022-11-22 09:06:40 +0100

    Add project files.

---
+++ TestProjectPearl/BandTest.cs
@@ -0,0 +1,169 @@
+﻿using BandOfPearl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProjectPearl
+{
+
+
+    /// <summary>
+    ///This is a test class for PearlNecklaceTest and is intended
+    ///to contain all PearlNecklaceTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class BandTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        [TestMethod()]
+        public void B01_Constructor()
+        {
+            Band band = new Band();
+            Assert.AreEqual(0, band.Count);
+        }
+
+
+        [TestMethod()]
+        public void B02_AddOne()
+        {
+            Band band = new Band();
+            band.AddPearl(new Pearl("Red", 1.5));
+            Assert.AreEqual(1, band.Count);
+        }
+
+        [TestMethod()]
+        public void B03_AddOneGetAtPosition()
+        {
+            Band band = new Band();
+            Pearl pearl = new Pearl("Red", 1.5);
+            band.AddPearl(pearl);
+            Pearl getPearl = band.GetPearlAtPosition(0);
+            Assert.AreSame(pearl, getPearl);
+        }
+
+        [TestMethod()]
+        public void B04_AddOneGetColor()
+        {
+            Band band = new Band();
+            Pearl pearl = new Pearl("Red", 1.5);
+            band.AddPearl(pearl);
+            int count = band.GetNumberOfColoredPearls("Red");
+            Assert.AreEqual(1, count);
+            count = band.GetNumberOfColoredPearls("Blue");
+            Assert.AreEqual(0, count);
+        }
+
+        [TestMethod()]
+        public void B05_AddManyGetColor()
+        {
+            Band band = new Band();
+            Pearl pearl = new Pearl("Red", 1.5);
+            band.AddPearl(pearl);
+            band.AddPearl(new Pearl("Blue", 2));
+            band.AddPearl(new Pearl("Blue", 3));
+            band.AddPearl(new Pearl("Red", 4.0));
+            band.AddPearl(new Pearl("Green", 5.1));
+            band.AddPearl(new Pearl("Blue", 6));
+            band.AddPearl(new Pearl("Red", 7));
+            int count = band.GetNumberOfColoredPearls("Red");
+            Assert.AreEqual(3, count);
+            count = band.GetNumberOfColoredPearls("Blue");
+            Assert.AreEqual(3, count);
+            count = band.GetNumberOfColoredPearls("Green");
+            Assert.AreEqual(1, count);
+        }
+
+        [TestMethod()]
+        public void B06_AddManyGetAtPosition()
+        {
+            Band band = new Band();
+            Pearl pearl = new Pearl("Red", 1.5);
+            band.AddPearl(pearl);
+            band.AddPearl(new Pearl("Blue", 2));
+            band.AddPearl(new Pearl("Blue", 3));
+            band.AddPearl(new Pearl("Red", 4.0));
+            band.AddPearl(new Pearl("Green", 5.1));
+            band.AddPearl(new Pearl("Blue", 6));
+            band.AddPearl(new Pearl("Red", 7));
+            Assert.AreEqual(7, band.Count);
+            Pearl getPearl = band.GetPearlAtPosition(0);
+            Assert.AreEqual("Red", getPearl.Color);
+            getPearl = band.GetPearlAtPosition(4);
+            Assert.AreEqual("Blue", getPearl.Color);
+            getPearl = band.GetPearlAtPosition(6);
+            Assert.AreSame(pearl, getPearl);
+        }
+
+        [TestMethod()]
+        public void B07_AddManyGetFailure()
+        {
+            Band band = new Band();
+            Pearl pearl = new Pearl("Red", 1.5);
+            band.AddPearl(pearl);
+            band.AddPearl(new Pearl("Blue", 2));
+            band.AddPearl(new Pearl("Blue", 3));
+            band.AddPearl(new Pearl("Red", 4.0));
+            band.AddPearl(new Pearl("Green", 5.1));
+            band.AddPearl(new Pearl("Blue", 6));
+            band.AddPearl(new Pearl("Red", 7));
+            Assert.AreEqual(7, band.Count);
+            Pearl getPearl = band.GetPearlAtPosition(7);
+            Assert.IsNull(getPearl);
+            getPearl = band.GetPearlAtPosition(-1);
+            Assert.IsNull(getPearl);
+            int count = band.GetNumberOfColoredPearls(null);
+            Assert.AreEqual(0, count);
+            band.AddPearl(null);
+            Assert.AreEqual(7, band.Count);
+        }
+
+    }
+}
---
+++ TestProjectPearl/PearlTest.cs
@@ -0,0 +1,91 @@
+﻿using BandOfPearl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProjectPearl
+{
+    
+    
+    /// <summary>
+    ///This is a test class for PearlTest and is intended
+    ///to contain all PearlTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class PearlTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        [TestMethod()]
+        public void P01_ConstructorSimple()
+        {
+            Pearl pearl = new Pearl("Red", 1.7);
+            Assert.AreEqual("Red", pearl.Color);
+            Assert.AreEqual(1.7, pearl.Weight);
+        }
+
+        [TestMethod()]
+        public void P02_ConstructorFailure()
+        {
+            Pearl pearl = new Pearl("Yellow", -10);
+            Assert.AreEqual("Unknown", pearl.Color);
+            Assert.AreEqual(0, pearl.Weight);
+        }
+
+        [TestMethod()]
+        public void P03_ConstructorNullFailure()
+        {
+            Pearl pearl = new Pearl(null, -10);
+            Assert.AreEqual("Unknown", pearl.Color);
+            Assert.AreEqual(0, pearl.Weight);
+        }
+    }
+}
